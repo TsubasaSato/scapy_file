84b21af84d6d565cfb05c93c0c9755ab21061404
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-12-05 22:09:22 +0900

    Update send_pkt.py

--- send_pkt.py
+++ send_pkt.py
@@ -11,6 +11,7 @@ dport = int(sys.argv[3])
 # SYN
 ip=IP(src=src,dst=dst)
 SYN=TCP(sport=sport,dport=dport,flags='S',seq=1000)
+# reply packet may be syn=1,Ack=1,AckNo=1001,seq=0(random)
 SYNACK=sr1(ip/SYN)
 
 # ACK
@@ -19,8 +20,10 @@ send(ip/ACK)
 
 time.sleep(15)
 
+# First Ack pkt after 3 way hand shake.
+data = "TCP connect is succesful!!"
 ip = IP(src=src, dst=dst)
-tcp = ip / TCP(sport=sport, dport=dport, flags="PA", seq=123, ack=1) / "scapy packet 123"
+tcp = ip / TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack, ack=SYNACK.seq+1) / data
 tcp.show2()
 
 send(tcp)
