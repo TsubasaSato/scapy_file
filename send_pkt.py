bc83ba91c930f8546fd5a1073b9b9f0731db4814
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-12-06 03:22:25 +0900

    Update send_pkt.py

--- send_pkt.py
+++ send_pkt.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
+#!/usr/share/python3
 from scapy.all import *
-import time
 
 # VARIABLES
 src = sys.argv[1]
@@ -11,19 +10,22 @@ dport = int(sys.argv[3])
 # SYN
 ip=IP(src=src,dst=dst)
 SYN=TCP(sport=sport,dport=dport,flags='S',seq=1000)
-# reply packet may be syn=1,Ack=1,AckNo=1001,seq=0(random)
 send(ip/SYN)
 
-# ACK
-ACK=TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack, ack=SYNACK.seq + 1)
-send(ip/ACK)
+# RST
+RST=SYN.flags='R'
+send(ip/RST)
 
-time.sleep(15)
+# SYN
+SYNACK=sr1(ip/SYN)
+# reply packet may be syn=1,Ack=1,AckNo=1001,seq=0(random)
+# ACK
+ACK=TCP(sport=sport, dport=dport, flags='A', seq=1001, ack=SYNACK.seq + 1)
+SYNACK=sr1(ip/ACK)
 
 # First Ack pkt after 3 way hand shake.
 data = "TCP connect is succesful!!"
 ip = IP(src=src, dst=dst)
-tcp = ip / TCP(sport=sport, dport=dport, flags='A', seq=SYNACK.ack, ack=SYNACK.seq+1) / data
-tcp.show2()
-
+tcp = ip / TCP(sport=sport, dport=dport, flags='A', seq=1, ack=1) / data
 send(tcp)
+ls(tcp)
