0eb093bd4e76866a9abff4eddc34156e97f67c6d
Tsubasa <43230360+TsubasaSato@users.noreply.github.com>
2019-12-05 19:11:59 +0900

    Create receiver.py

---
+++ receiver.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import socket
+from scapy.all import *
+
+ip = sys.argv[1]
+port = sys.argv[2]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((ip, int(port)))
+s.listen(1)
+
+while True :
+    conn, addr = s.accept()
+    print('Connection address:', addr)
+    data = conn.recv(1024) #
